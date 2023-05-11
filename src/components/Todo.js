e69461e37209988bc0bf3b594f7c3e2fdb80a601
mehimdate22 <mehimdate22@gmail.com>
2023-05-11 03:48:54 +0100

    add todos and using props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = () => {
+const Todo = ( { task } ) => {
   return (
-    <div>Todo</div>
+    <div className='Todo'>
+        <p>{task.task}</p>
+        <div>
+            <FontAwesomeIcon icon={faPenToSquare}/>
+            <FontAwesomeIcon icon={faTrash}/>
+        </div>
+    </div>
   )
 }
+
+export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const TodoForm = () => {
-  return (
-    <div>TodoForm</div>
-  )
+export const TodoForm = ( { addTodo } ) => {
+
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        addTodo(value);
+        setValue('');
+        
+    }
+
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text'
+                className='todo-input'
+                placeholder='what is the tasks for today'
+                value={value}
+                onChange={(e)=>setValue(e.target.value)} />
+            <button type='submit' className='todo-btn'>Add Task</button>
+        </form>
+    )
 }
