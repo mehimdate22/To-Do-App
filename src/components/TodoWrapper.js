3a1bbe8be5d06e4d8466f38bc9002d77b973e809
mehimdate22 <mehimdate22@gmail.com>
2023-05-11 04:29:28 +0100

    deleting todo

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -11,15 +11,24 @@ const TodoWrapper = () => {
     const addTodo = (todo) =>{
         setTodos([...todos,{id:uuidv4(), task : todo,
         completed: false , isEditing: false}]);
-        console.log(todos);
-        
+        console.log(todos);   
+    }
+    const toggleComplete = (id) =>{
+        setTodos(todos.map(todo => todo.id === id ? {...todo,completed : !todo.completed }: todo))
+    }
+
+    const deleteTodo = (id) =>{
+        setTodos(todos.filter(todo => todo.id !== id))
     }
   return (
     <div className='TodoWrapper'>
         <h1>Get Things Done !</h1>
         <TodoForm addTodo = {addTodo}/>
         {todos.map((todo,index)=>(
-            <Todo task = {todo} key={index}/>
+            <Todo task = {todo} 
+            key={index} 
+            toggleComplete={toggleComplete}
+            deleteTodo = {deleteTodo}/>
         ))}
     </div>
   )
